fcb4ed2029ad880a79cf69d30e1844043b93b1d5
ffjaervik <fabianfjaervik@gmail.com>
2023-06-21 12:54:17 +0100

    1.5 completed

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Part } from "./Part";
 
 export const Content = (props) => {
+      console.log('the props', props)
   return (
     <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
+      <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
+      <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
     </>
   );
 };
--- part1/courseinfo/src/components/Total.js
+++ part1/courseinfo/src/components/Total.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const Total = (props) => {
   console.log(props)
   return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
   )
 }
