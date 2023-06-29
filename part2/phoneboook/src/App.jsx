64579897424d8d85ca5ad47259709552f113d5a3
ffjaervik <fabianfjaervik@gmail.com>
2023-06-29 12:48:16 +0100

    2.11 complete

--- part2/phoneboook/src/App.jsx
+++ part2/phoneboook/src/App.jsx
@@ -2,21 +2,36 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Person from "./components/Person";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [personFilter, setpersonFilter] = useState("");
   const allNames = persons.map((person) => person.name);
-  console.log(allNames);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+  }, []);
+
+  // EXAMPLE OF DATA FETCHING WITHOUT AXIOS
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const res = await fetch("http://localhost:3001/persons");
+  //       const data = await res.json();
+  //       setPersons(data);
+  //     } catch (error) {
+  //       console.log("error", error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
 
   const handleNameChange = (e) => setNewName(e.target.value);
   const handleNumberChange = (e) => setNewNumber(e.target.value);
