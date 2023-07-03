0a114d95d92b256af611dcc2de036231dd57c109
ffjaervik <fabianfjaervik@gmail.com>
2023-07-03 14:55:13 +0100

    3.3 complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const personsData = [
   {
     id: 1,
@@ -40,18 +42,22 @@ app.get("/info", (req, res) => {
   );
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const reqPerson = personsData.find((person) => person.id === id);
+  if(reqPerson) {
+      res.json(reqPerson)
+  } else{
+      res.status(404).end()
+  }
+
+});
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-//     app.get('/', (req, res) => {
-//       res.send('<h1>Hello World!</h1>')
-//     })
-
-//     app.get('/api/notes', (req, res) => {
-//       res.json(notes)
-//     })
 
 //     const generateId = () => {
 //       const maxId = notes.length > 0
@@ -81,17 +87,6 @@ app.listen(PORT, () => {
 //       response.json(note)
 //     })
 
-//     app.get('/api/notes/:id', (request, response) => {
-//       const id = Number(request.params.id)
-//       const note = notes.find(note => note.id === id)
-
-//       if (note) {
-//         response.json(note)
-//       } else {
-//         response.status(404).end()
-//       }
-
-//     })
 
 //     app.delete('/api/notes/:id', (request, response) => {
 //       const id = Number(request.params.id)
