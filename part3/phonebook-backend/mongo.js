7af6d9619d81eda1538867b471e674b3521b6c79
ffjaervik <fabianfjaervik@gmail.com>
2023-07-06 18:36:35 +0100

    3.13 completed

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+console.log("process.arg", process.argv);
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fabianfjaervik:${password}@cluster0.bit7b16.mongodb.net/PhonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log("person saved!", result);
+    mongoose.connection.close();
+  });
+}
