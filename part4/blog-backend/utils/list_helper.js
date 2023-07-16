67653e241f1751565e97196af6c6a22f97603632
ffjaervik <fabianfjaervik@gmail.com>
2023-07-16 12:20:15 +0200

    4.4 completed

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -1,5 +1,12 @@
-const dummy = (blogs) =>{
-      return 1
-      }
-      
-      export default dummy
+const dummy = (blogs) => {
+  return 1
+}
+const totalLikes = (blogs) => {
+      return blogs.reduce((prev, curr) => prev + curr.likes, 0)
+//   let likes = 0
+//   blogs.map((blog) => {
+//     return (likes = likes += blog.likes)
+//   })
+//   return likes
+}
+export { dummy, totalLikes }
