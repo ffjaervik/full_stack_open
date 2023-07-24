18544ed121455025475f7c9ceaa02fdde730cc34
ffjaervik <fabianfjaervik@gmail.com>
2023-07-24 15:30:39 +0100

    4.8 completed

--- part4/blog-backend/controllers/blogs.js
+++ part4/blog-backend/controllers/blogs.js
@@ -1,26 +1,29 @@
+// require('express-async-errors') // This package will take care of errors in async functions. Basically ut gets rid of the need for try-catch blocks in async functions.
 const blogsRouter = require('express').Router()
-const Blog = require('../models/blog.js')
+const BlogModel = require('../models/blog.js')
 
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await BlogModel.find({})
+  response.json(blogs)
+})
 
-blogsRouter.get('/', (request, response) => {
-console.log('get request')
-  Blog.find({}).then((blogs) => {
-      console.log('blogs', blogs)
-    response.json(blogs)
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const blog = new BlogModel({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
   })
-})
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-    .catch((error) => {
-      response.status(400).json(error)
-    })
-})
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+    
+  } catch (exception) {
+    next(exception)
+  }
+  })
 
 module.exports = blogsRouter
