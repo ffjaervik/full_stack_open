5d7529392f62f8cc4f5f3f7f6eae62a23167f72a
ffjaervik <fabianfjaervik@gmail.com>
2023-07-24 16:29:14 +0100

    4.9 completed

--- part4/blog-backend/tests/note_api.test.js
+++ part4/blog-backend/tests/note_api.test.js
@@ -6,7 +6,6 @@ const initialBlogs = require('./test_helper')
 
 const api = supertest(app)
 
-
 beforeEach(async () => {
   await BlogModel.deleteMany({})
   await BlogModel.insertMany(initialBlogs)
@@ -19,13 +18,14 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test(
-  'all blogs are returned',
-  async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length)
-  }
-)
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+test('the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
