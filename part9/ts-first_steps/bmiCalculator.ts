3b50c33178d82111d8cecd4ea72aed451c871511
ffjaervik <fabianfjaervik@gmail.com>
2023-08-08 14:29:17 +0200

    9.3 completed

--- part9/ts-first_steps/bmiCalculator.ts
+++ part9/ts-first_steps/bmiCalculator.ts
@@ -13,4 +13,29 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-console.log(calculateBmi(180, 74))
+const parseBmiArguments = (args: Array<string>): Array<number> => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+
+  const height = Number(args[2])
+  const weight = Number(args[3])
+
+  if (!isNaN(height) && !isNaN(weight)) {
+    return [height, weight]
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+try {
+  const [height, weight] = parseBmiArguments(process.argv)
+  console.log(calculateBmi(height, weight))
+} catch (e) {
+  let errorMessage = 'Something went wrong'
+  if (e instanceof Error) {
+    errorMessage = e.message
+  }
+  console.log(errorMessage)
+}
+
+// console.log(calculateBmi(180, 74))
--- part9/ts-first_steps/exerciseCalculator.ts
+++ part9/ts-first_steps/exerciseCalculator.ts
@@ -13,7 +13,7 @@ const calculateExercises = (hours: Array<number>, target: number): Result => {
   const trainingDays: number = hours.filter((h) => h > 0).length
   const average: number = hours.reduce((a, b) => a + b) / periodLength
   const success: boolean = average >= target
-  const rating: number = average > target ? 3 : average === target ? 2 : 1
+  const rating: number = average >= target ? 3 : average >= target / 2 ? 2 : 1
   const ratingDescription: string =
     rating === 3
       ? '3 out of 3: great'
@@ -32,4 +32,27 @@ const calculateExercises = (hours: Array<number>, target: number): Result => {
   }
 }
 
-console.log(calculateExercises([4, 3, 3, 3, 3, 3, 3], 3))
+const parseExerciseArguments = (args: Array<string>): Array<number> => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  const target = Number(args[2])
+  const hours = args.slice(3).map((h) => Number(h))
+  if (!isNaN(target) && hours.every((h) => !isNaN(h))) {
+    return [target, ...hours]
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+try {
+  const [target, ...hours] = parseExerciseArguments(process.argv)
+  console.log(calculateExercises(hours, target))
+} catch (e) {
+  let errorMessage = 'Something went wrong'
+  if (e instanceof Error) {
+    errorMessage = e.message
+  }
+  console.log(errorMessage)
+}
+
+
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
