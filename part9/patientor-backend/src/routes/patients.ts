cd0dc7b851f40874092fad1aa5eb9fe44d5bf031
ffjaervik <fabianfjaervik@gmail.com>
2023-08-22 19:43:18 +0200

    9.12 WIP

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,12 +1,17 @@
 import express from 'express';
-import {getNonSensitivePatients} from '../services/patientsService';
+import { getNonSensitivePatients } from '../services/patientsService';
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (_req, res) =>{
-      const result = getNonSensitivePatients()
-      console.log('someone requested patient data')
-      res.send(result)
-})
+router.get('/', (_req, res) => {
+  const result = getNonSensitivePatients();
+  console.log('someone requested patient data');
+  res.send(result);
+});
+
+router.post('/', (req, res) => {
+  const newPatient: = req.body
+  res.send('Saving a patient!');
+});
 
 export default router;
